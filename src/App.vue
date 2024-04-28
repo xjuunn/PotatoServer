295406f5e5166f624b822e1d58c1405e80953eab
xjuunn <145442547+xjuunn@users.noreply.github.com>
2024-04-28 22:22:47 +0800

    install

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
- PotatoServer
+    <el-button @click="test">test</el-button>
+    <div class="test">test</div>
 </template>
 <script setup>
+import anime from 'animejs';
+function test(){
+console.log(anime);
+anime({
+  targets: '.test',
+  translateX: 250
+});
+}
 
 </script>
 <style scoped>
-
+    .test{
+        background-color: red;
+        display: inline-block;
+    }
 </style>
