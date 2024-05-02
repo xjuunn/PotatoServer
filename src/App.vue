47de1c08ea34371a3be0bd70a8c6358f837ec4b6
xjuunn <145442547+xjuunn@users.noreply.github.com>
2024-05-02 17:44:18 +0800

    添加皮肤模型组件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <IndexBg icon="/favicon.ico" txtsvg="/favicontxt.svg"></IndexBg>
 
-
+  <skin-view :config="config" @onGetViewer="test"></skin-view>
 </template>
 <script setup>
 import IndexBg from "./components/IndexBg.vue";
+import SkinView from "./components/skinView.vue";
+let config = {
+  width:400,
+  height:500,
+  skin:"/skins/_xjuunn.png"
+}
+function test(e){
+  console.log(e)
+  e.zoom = 0.33;
+  e.nameTag = "_xjuunn"
+  // e.camera = false;
 
-
+}
 
 
 </script>
---
+++ src/components/skinView.vue
@@ -0,0 +1,21 @@
+<template>
+    <canvas id="skin_container"></canvas>
+</template>
+<script setup>
+import * as skinview3d from 'skinview3d';
+let emit = defineEmits(['onGetViewer'])
+let props = defineProps(['config'])
+import {onMounted} from "vue";
+onMounted(()=>{
+  let skinViewer = new skinview3d.SkinViewer({
+    canvas:document.getElementById("skin_container"),
+    width: props?.config?.width??300,
+    height: props?.config?.height??400,
+    skin: props?.config?.skin??"/skins/_xjuunn.png"
+  })
+  emit('onGetViewer',skinViewer)
+})
+</script>
+<style scoped>
+
+</style>
