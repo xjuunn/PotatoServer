d9bc481e6a788075b8fd99580415dcd876c135e3
xjuunn <145442547+xjuunn@users.noreply.github.com>
2024-05-06 21:58:35 +0800

    首页添加皮肤模型

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
     <div class="serverDomain">
       <span v-for="(item,index) in config.serverInfo.domain" :key="index">{{item}}</span>
     </div>
+    <skin-view class="skinView" :config="skinConfig" @onGetViewer="initSkinView"></skin-view>
   </div>
-  <!--  <skin-view class="border skinView" :config="config" @onGetViewer="test"></skin-view>-->
 </template>
 <script setup>
 import IndexBg from "./components/IndexBg.vue";
@@ -14,28 +14,32 @@ import SkinView from "./components/skinView.vue";
 import config from './config.js';
 import * as skinView3d from 'skinview3d';
 import anime from "animejs";
-import {onMounted} from "vue";
-let viewer ;
-let Skinconfig = {
-  width:400,
-  height:500,
+import {onMounted, ref} from "vue";
+let color1 = config.serverInfo.colorTheme1;
+let color2 = config.serverInfo.colorTheme2;
+let skinConfig = {
+  width:900,
+  height:900,
   skin:"/skins/_xjuunn.png"
 }
 onMounted(()=>{
-  // var skinViewAnime = anime.timeline({
-  //   easing:'easeInOutQuad',
-  //   update:function () {  }
-  // })
-  // skinViewAnime.add({
-  //   targets:''
-  // });
+  setSkinViewSize();
+  setSkinViewSize();
+  setAnimes();
+
+})
+// 设置页面动画
+function setAnimes(){
   let indexAnime = anime.timeline({});
+  indexAnime.add({
+    targets:'.serverName',
+  },2)
   indexAnime.add({
     targets: '.serverName',
     top: '20vh',
     opacity:1,
     delay: anime.stagger(20, {from: 'center'})
-  })
+  },'-=20')
   indexAnime.add({
     targets:'.serverDomain span',
     right:'0vw',
@@ -44,15 +48,48 @@ onMounted(()=>{
     delay: function (e) {
       return anime.random(-300,300);
     }
-  },'-=800')
-
-})
-
-function test(skinViewer){
-  skinViewer.animation = new skinView3d.FlyingAnimation();
-  skinViewer.camera.position.set(...[-80,-20,20]);
-  viewer = skinViewer;
+  },'-=1000')
+  indexAnime.add({
+    targets: '.serverName',
+    rotate: anime.stagger([-360, 360]),
+    duration:20,
+    delay:anime.stagger(20, {from: 'center'}),
+  })
+  // indexAnime.add({
+  //   targets:'.skinView',
+  //   top:0,
+  //   left:0,
+  //   easing:'linear'
+  // })
 }
+// 初始化皮肤视图
+let skinCamera;
+let skinView;
+function initSkinView(skinView1){
+  skinView = skinView1;
+  skinCamera = skinView.camera;
+  skinView.animation = new skinView3d.FlyingAnimation();
+  skinView.controls.enableRotate = true;
+  skinView.scene.children[2].position.set(0,0,-20);
+
+}
+// SkinView 响应式
+window.onresize = reSize;
+function reSize(){
+  setSkinViewSize();
+}
+function setSkinViewSize(){
+  const {innerWidth,innerHeight} = window;
+  skinView.renderer.setSize(innerWidth,innerHeight);
+  let offset = -(innerWidth-1400)*0.05;
+  console.log(offset)
+  skinView.scene.children[2].position.y = offset;
+  skinCamera.aspect = innerWidth / innerHeight;
+  skinCamera.updateProjectionMatrix();
+  skinCamera.scale.x = skinCamera.scale.y = skinCamera.z = 3000/innerWidth;
+  skinCamera.fov =30;
+}
+
 
 </script>
 <style scoped>
@@ -63,6 +100,7 @@ function test(skinViewer){
 .body{
   width: 100vw;
   height: 100vh;
+  overflow-x: hidden;
 }
 .serverName{
   font-family: Pixel,sans-serif;
@@ -71,6 +109,11 @@ function test(skinViewer){
   position: relative;
   top: -25vh;
   opacity: 0;
+  text-shadow: 3px 3px 9px #000 ,
+  3px 3px 3px #000,
+  3px 3px 3px #000,
+  6px 6px 10px #000,
+  2px 2px 3px #000;
 }
 .serverDomain{
   font-family: Pixel,sans-serif;
@@ -80,34 +123,41 @@ function test(skinViewer){
   margin: 0 auto;
   width: 70vw;
   text-align: right;
+  text-shadow: 1px 1px 4px #000 ,
+  1px 1px 1px #000;
 }
 .serverDomain span{
   position: relative;
   right: -100vw;
   opacity: 0;
 }
+/* .serverName:nth-child(n+5){
+  z-index: 6;
+} */
+.skinView{
+  position: absolute;
+  top:0;
+  left: 0;
+  z-index: 5;
+  transform-origin: left top;
+}
 
-/* 媒体查询：针对小型设备（手机）设置不同的字体大小 */
+
 @media (max-width: 576px) {
   .serverDomain {
-    font-size: 20px; /* 在小型设备上设置较小的字体大小 */
+    font-size: 20px;
   }
 }
-/* 媒体查询：针对中型设备（平板）设置不同的字体大小 */
 @media (min-width: 576px) {
   .serverDomain {
-    font-size: 30px; /* 在平板设备上设置较大的字体大小 */
+    font-size: 30px;
   }
 }
-
-
-/* 媒体查询：针对大型设备（桌面）设置不同的字体大小 */
 @media (min-width: 992px) {
   .serverDomain {
-    font-size: 40px; /* 在桌面设备上设置更大的字体大小 */
+    font-size: 40px;
   }
 }
-
 </style>
 <style>
 @font-face {
--- src/config.js
+++ src/config.js
@@ -2,6 +2,8 @@ const config = {
     serverInfo:{
         name:"PotatoMC",
         domain:"potatomc.example",
+        colorTheme1:'#F3BE5AED',// 浅主题色
+        colorTheme2:'#D7A952ED',// 深主题色
     }
 }
 
